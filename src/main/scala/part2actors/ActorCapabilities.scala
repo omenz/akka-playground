ab07ea17aa9623163d4b9127b97cf8cbcc1a3c4f
alex.k <alex.k>
2018-12-12 20:46:59 +0200

    actor capabilities

---
+++ src/main/scala/part2actors/ActorCapabilities.scala
@@ -0,0 +1,58 @@
+package part2actors
+
+import akka.actor.{Actor, ActorRef, ActorSystem, Props}
+
+object ActorCapabilities extends App {
+
+  class SimpleActor extends Actor {
+    override def receive: Receive = {
+      case "Hi!" => sender() ! "Hello, there!" // or context.sender(), replying to a message
+      case message: String => println(s"[${context.self.path}] I have received: $message")
+      case number: Int => println(s"[simple actor] I have received a NUMBER: $number")
+      case SpecialMessage(contents) => println(s"[simple actor] I have received something SPECIAL: $contents")
+      case SendMessageToYourself(content) => self ! content
+      case SayHiTo(ref) => ref ! "Hi!"
+      case WirelessPhoneMessage(content, ref) => ref forward (content + "s") // I keep the original sender of WirelessPhoneMessage
+    }
+  }
+
+  val system = ActorSystem("actorCapabilitiesDemo")
+  val simpleActor = system.actorOf(Props[SimpleActor], "simpleActor")
+
+  simpleActor ! "hello, actor"
+
+  // 1 - messages can be of any type
+  // a) messages must be IMMUTABLE
+  // b) messages must be SERIALIZABLE
+  // in practice use case classes and case objects
+  simpleActor ! 42 // who is the sender?
+
+  case class SpecialMessage(contents: String)
+  simpleActor ! SpecialMessage("some special message")
+
+  // 2 - actors have information about their context and them selves
+  // context.self === `this` in OOP
+
+  case class SendMessageToYourself(content: String)
+  simpleActor ! SendMessageToYourself("I am an actor and I am proud of it!")
+
+  // 3 - actors can REPLY to messages
+  val alice = system.actorOf(Props[SimpleActor], "alice")
+  val bob = system.actorOf(Props[SimpleActor], "bob")
+
+  case class SayHiTo(ref: ActorRef)
+  alice ! SayHiTo(bob)
+
+  // 4 - dead letters
+  alice ! "Hi!" // reply to "me", goes straight to dead letters
+
+  // 5 - forwarding messages
+  // D -> A -> B
+  // forwarding = sending a message with the ORIGINAL sender
+
+  case class WirelessPhoneMessage(content: String, ref: ActorRef)
+  alice ! WirelessPhoneMessage("Hi", bob) //noSender
+
+  Thread.sleep(1000)
+  System.exit(0)
+}
